07b8830cb2d16bedcc7440800873856313fb5210
hziegel <hziegel@gmail.com>
2018-06-26 11:48:53 -0400

    Getting down ideas

--- src/Maze.java
+++ src/Maze.java
@@ -1,18 +1,39 @@
 
 public class Maze {
 
-	int x;
-	int y;
-	int[][] grid;
+	int x_size;
+	int y_size;
+	char[][] grid;
+	int[] startTile;
+	int[] endTile;
 	
 	public void createGrid(int x,int y) {
 		if(x!=0 & y!=0 & x%6==0 & y%6==0) {
-			this.x = x;
-			this.y = y;
-			grid = new int[x][y];
+			x_size = x;
+			y_size = y;
+			grid = new char[x][y];
 		}
 	}
 	
+	public void fillGrid() {
+		for(char[] each : grid) {
+			for(char item : each) {
+				item = 'O';
+			}
+		}
+	}
+	
+	public void randomTile() {
+		
+	}
+	
+	public void checkInGrid(int xOrY) {
+		
+	}
+	
+	public void checkIfFinish() {
+		
+	}
 	
 	
 }
