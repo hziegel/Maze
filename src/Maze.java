3416c7e1d55d709fe587c0c545c319c8152f524d
hziegel <hziegel@gmail.com>
2018-06-25 20:11:40 -0400

    Setting up and create grid

--- src/Maze.java
+++ src/Maze.java
@@ -1,4 +1,18 @@
 
 public class Maze {
 
+	int x;
+	int y;
+	int[][] grid;
+	
+	public void createGrid(int x,int y) {
+		if(x!=0 & y!=0 & x%6==0 & y%6==0) {
+			this.x = x;
+			this.y = y;
+			grid = new int[x][y];
+		}
+	}
+	
+	
+	
 }
